e367bca62c2f2cbcdf9888492359c622ef74e3e1
Daniel Brennan <dannyjb123@gmail.com>
2017-02-25 14:07:50 -0500

    Update contentScript.js

---
+++ contentScript.js
@@ -0,0 +1,19 @@
+Var count = 0;
+Var url = “”;
+Var user = “stackoverflow.com”;
+
+/*//permission to play with tabs
+"permissions": [ "tabs" ]
+
+//get url and store that jawn in yo variable
+chrome.tabs.getSelected(null,function(tab) {
+	var url = tab.url; 
+});*/
+
+url = "http://stackoverflow.com/questions/42459444/difference-between-i-i-and-i-i-1"
+//this should compare to see if the users input is contained in the current url
+if(url.indexOf(user) > -1){
+//if what the user is looking for is found in the url increment the count
+	count++;
+	consolelog(count);
+}
